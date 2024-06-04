521fc5919f7610968fee901e5d2d914a7b2d1b34
Albin <albin.pilenas@gmail.com>
2024-06-04 11:48:32 +0200

    menu

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import menuRouter from './routes/menu.js';
 
 const app = express();
 const PORT = 8080;
@@ -9,10 +10,10 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-
+app.use('/menu', menuRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}...`);
 });
 
 // Middleware för felhantering
