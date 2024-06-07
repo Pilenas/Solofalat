0e05dad916a1af6905632c0ac7024644e64af083
Albin <albin.pilenas@gmail.com>
2024-06-07 15:34:11 +0200

    orders

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import cartRoutes from './routes/cart.js';
 import menuRouter from './routes/menu.js';
-import aboutRouter from "./routes/about.js";
+import aboutRouter from './routes/about.js';
 import authRouter from './routes/auth.js';
+import orderRouter from './routes/orders.js';
 
 const app = express();
 const PORT = 8080;
@@ -15,11 +16,16 @@ app.use(cors());
 // Routes
 app.use('/api/cart', cartRoutes);
 app.use('/menu', menuRouter);
-app.use("/about", aboutRouter);
+app.use('/about', aboutRouter);
 app.use('/auth', authRouter);
+app.use('/order', orderRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
 });
 
 // Middleware för felhantering
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
